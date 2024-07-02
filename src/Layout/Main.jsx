df826481f6f5c9d52b431a59b3d6ec20980de778
alamin6688 <alamin.kzs06@gmail.com>
2024-07-03 02:02:25 +0600

    Footer section added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Pages/Shared/Navbar/Navbar";
+import Footer from "../Pages/Shared/Footer/Footer";
 
 
 const Main = () => {
@@ -9,6 +10,7 @@ const Main = () => {
                 <Navbar></Navbar>
             </div>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
