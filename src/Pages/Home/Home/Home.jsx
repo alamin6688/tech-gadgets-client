7a57f205e319b18701c7068f38138384c0fc0db6
alamin6688 <alamin.kzs06@gmail.com>
2024-07-04 00:19:40 +0600

    Banner section added

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,11 @@
+import Banner from "../Banner/Banner";
 import Reviews from "../Reviews/Reviews";
 
 
 const Home = () => {
     return (
         <div className="min-h-[calc(100vh-382px)]">
+            <Banner></Banner>
             <Reviews></Reviews>
         </div>
     );
